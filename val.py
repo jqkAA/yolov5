b7f73f4193d1587cd141e86beac929f67aa6fb2d
jqkAA <96966030+jqkAA@users.noreply.github.com>
2022-01-17 14:07:32 +0700

    Update val.py

--- val.py
+++ val.py
@@ -266,6 +266,7 @@ def run(data,
     if (verbose or (nc < 50 and not training)) and nc > 1 and len(stats):
         for i, c in enumerate(ap_class):
             LOGGER.info(pf % (names[c], seen, nt[c], p[i], r[i], ap50[i], ap[i]))
+            print(pf % (names[c], seen, nt[c], p[i], r[i], ap50[i], ap[i]))
 
     # Print speeds
     t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
