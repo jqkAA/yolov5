4a144f0627cd81e0b95222407fcf3dda8c697903
jqkAA <96966030+jqkAA@users.noreply.github.com>
2022-01-22 21:44:01 +0700

    Update val.py

--- val.py
+++ val.py
@@ -261,6 +261,7 @@ def run(data,
     # Print results
     pf = '%20s' + '%11i' * 2 + '%11.3g' * 4  # print format
     LOGGER.info(pf % ('all', seen, nt.sum(), mp, mr, map50, map))
+    print(pf % ('all', seen, nt.sum(), mp, mr, map50, map))
 
     # Print results per class
     if (verbose or (nc < 50 and not training)) and nc > 1 and len(stats):
